bbfb99a23196241658bb8970363fdcf9d74f99f2
kattata <kasia.laniecka@gmail.com>
2026-04-04 05:01:51 +0200

    Map

--- components/base/map-embed.tsx
+++ components/base/map-embed.tsx
@@ -1,4 +1,6 @@
 export function MapEmbed() {
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_PLATFORM_API_KEY;
+
   return (
     <iframe
       width="100%"
@@ -6,7 +8,7 @@ export function MapEmbed() {
       style={{ border: 0 }}
       loading="lazy"
       allowFullScreen
-      src="https://www.google.com/maps/embed/v1/place?q=place_id:EivFmndpxJl0b2phxYRza2EgNTkvNCwgODEtMzkxIEdkeW5pYSwgUG9sYW5kIh0aGwoWChQKEgl1X7NJMKf9RhHYxoB7LQv2lxIBNA&key=AIzaSyCaFRR-Aef-YoDizFeWgZ_C6KIIcYo8YW4"
+      src={`https://www.google.com/maps/embed/v1/place?q=place_id:EivFmndpxJl0b2phxYRza2EgNTkvNCwgODEtMzkxIEdkeW5pYSwgUG9sYW5kIh0aGwoWChQKEgl1X7NJMKf9RhHYxoB7LQv2lxIBNA&key=${apiKey}`}
     ></iframe>
   );
 }

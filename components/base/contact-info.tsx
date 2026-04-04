9bc4e5c4b82594b527edd76e81217380628a17ad
kattata <kasia.laniecka@gmail.com>
2026-04-04 05:06:05 +0200

    Add links to contact info

--- components/base/contact-info.tsx
+++ components/base/contact-info.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 type Props = {
   className?: string;
   showDividers?: boolean;
@@ -5,14 +7,18 @@ type Props = {
 
 export function ContactInfo({ className, showDividers }: Props) {
   return (
-    <div className={`flex flex-col ${className}`}>
+    <div className={`flex flex-col ${className} [&_a]:underline`}>
       <div
         className={
           showDividers ? "border-b border-b-gray-200 pb-4 mb-4" : "pb-4"
         }
       >
-        <div>+48 669 998 340</div>
-        <div>ga.finanse@wp.pl</div>
+        <div>
+          <Link href={"tel:+48 669 998 340"}>+48 669 998 340</Link>
+        </div>
+        <div>
+          <Link href={"mailto:ga.finanse@wp.pl"}>ga.finanse@wp.pl</Link>
+        </div>
       </div>
 
       <div

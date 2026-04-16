9a09ac6ae2291cfcd99f3934a465420cf4dc8c99
kattata <kasia.laniecka@gmail.com>
2026-04-16 20:48:03 +0200

    Fix mobile menu

--- components/global/header/mobile-header.tsx
+++ components/global/header/mobile-header.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { Menu } from "@deemlol/next-icons";
-
 import Link from "next/link";
 import { useState } from "react";
 import { Logo } from "../logo";
@@ -10,16 +9,20 @@ export function MobileHeader() {
   const navigation = useNavigation();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  function handleLinkClick() {
+    setIsMenuOpen(false);
+  }
+
   return (
     <>
       <header className="flex items-center justify-between gap-3 uppercase w-full container mx-auto py-5">
-        <Link href={"/"}>
+        <Link href={"/"} onClick={handleLinkClick}>
           <Logo />
         </Link>
 
         <button
           onClick={() => setIsMenuOpen(!isMenuOpen)}
-          className="text-primary-yellow relative z-30"
+          className="text-primary-yellow relative z-60"
         >
           <Menu color="currentColor" />
         </button>
@@ -28,18 +31,18 @@ export function MobileHeader() {
       <div
         className={
           isMenuOpen
-            ? "block px-4 py-5 bg-white absolute top-0 left-0 w-full h-screen"
+            ? "block px-4 py-5 bg-white absolute top-0 left-0 w-full h-screen z-50"
             : "hidden"
         }
       >
-        <Link href={"/"}>
+        <Link href={"/"} onClick={handleLinkClick}>
           <Logo />
         </Link>
 
         <nav className="flex flex-col gap-4 font-medium mt-10 uppercase">
           {navigation.map((item) => {
             return (
-              <Link key={item.url} href={item.url}>
+              <Link key={item.url} href={item.url} onClick={handleLinkClick}>
                 {item.label}
               </Link>
             );

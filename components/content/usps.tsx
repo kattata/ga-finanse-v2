6098439056b903f4f7a122f9323d52b3530f87af
kattata <kasia.laniecka@gmail.com>
2026-04-27 18:13:49 +0200

    Style USPs

--- components/content/usps.tsx
+++ components/content/usps.tsx
@@ -6,14 +6,17 @@ interface Props {
 
 export default function USPList({ usps }: Props) {
   return (
-    <ul className="!p-0 flex justify-center gap-10 md:gap-40 flex-wrap">
+    <ul className="!p-0 flex justify-center gap-10 md:gap-x-40 md:gap-y-20 flex-wrap">
       {usps.map((usp) => {
         return (
-          <li className="!list-none text-center" key={usp.headline}>
-            <div className="text-[40px] md:text-[54px] font-bold text-primary-navy">
+          <li
+            className="!list-none text-center items-center flex flex-col gap-3"
+            key={usp.headline}
+          >
+            <div className="text-[40px] md:text-[54px] font-bold text-primary-navy leading-12 md:leading-15">
               {usp.headline}
             </div>
-            <div className="uppercase font-semibold">{usp.text}</div>
+            <div className="font-semibold max-w-3/4 uppercase">{usp.text}</div>
           </li>
         );
       })}
